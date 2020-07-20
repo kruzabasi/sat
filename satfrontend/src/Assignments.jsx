b43ff3f04f1ef2e138b17fb89b18b95abd111ca7
kruzabasi <kruzabasi@gmail.com>
2020-07-20 09:57:26 +0100

    ui for application styled w/ bootstrap

---
+++ satfrontend/src/Assignments.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Assignments extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>hello</h1>
+      </div>
+    );
+  }
+}
+
+export default Assignments;
