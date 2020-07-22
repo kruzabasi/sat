176947f5dee72aea2c903eda7682fe6c74112045
kruzabasi <kruzabasi@gmail.com>
2020-07-22 15:43:54 +0100

    assingment(s) component

--- satfrontend/src/Assignments.jsx
+++ satfrontend/src/Assignments.jsx
@@ -1,16 +1,58 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Assignment from "./Assignment";
 
 class Assignments extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+    this.state = {
+      assignments: []
+    };
+    this.deleteAssignment = this.deleteAssignment.bind(this);
+  }
 
-    this.state = {};
+  componentDidMount() {
+    axios.get("http://localhost:5000/assignment/").then(response => {
+      this.setState({
+        assignments: response.data
+      });
+    });
+  }
+
+  deleteAssignment(id) {
+    axios
+      .delete("http://localhost:5000/assignment/" + id)
+      .then(response => console.log(response.data));
+    this.setState({
+      assignments: this.state.assignments.filter(astodel => astodel._id !== id)
+    });
+  }
+
+  assignmentStack() {
+    return this.state.assignments.map(singledOutAssignment => {
+      return (
+        <Assignment
+          assignment={singledOutAssignment}
+          deleteAssignment={this.deleteAssignment}
+          key={singledOutAssignment._id}
+        />
+      );
+    });
   }
 
   render() {
     return (
       <div>
-        <h1>hello</h1>
+        <table className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th>Student's Username</th>
+              <th>Teacher's Report</th>
+              <th>Submission Time</th>
+            </tr>
+          </thead>
+          <tbody>{this.assignmentStack()}</tbody>
+        </table>
       </div>
     );
   }
